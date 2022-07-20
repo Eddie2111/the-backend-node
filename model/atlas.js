a03df48cfa82a6d09917f5c0d5a9556d7469fbe0
ASM Tareq Mahmood <59513101+Eddie2111@users.noreply.github.com>
2022-07-20 13:40:07 +0600

    first commit
    check this function to draw out errors. Modified to print specific error message.

--- model/atlas.js
+++ model/atlas.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 const url = `mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@${process.env.DBCLUSTER}`;
 const connection = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('DB:200'))
-    .catch(err => console.error('DB:404', err));
+    .catch(err => console.error('DB:404', err.message));
 module.exports = connection;
 
